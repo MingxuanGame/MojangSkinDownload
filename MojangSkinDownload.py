d8d5ff9e33350c4ff8b2b9e4220bbb4b3529013a
MingxuanGame <68982190+MingxuanGame@users.noreply.github.com>
2020-09-30 22:11:09 +0800

    Update MojangSkinDownload.py

--- MojangSkinDownload.py
+++ MojangSkinDownload.py
@@ -1,5 +1,41 @@
 #coding:utf-8
-
+import os
+os.system('rd /s /q C:\\msdres')
+import random
+ran = random.randint(1,4)
+if ran == 1:
+    print('  __  __   _____  _____  ')
+    print(' |  \/  | / ____ |  __ \ ')
+    print(' | \  / |  (___  | |  | |')
+    print(' | |\/| | \___ \ | |  | |')
+    print(' | |  | | ____)  | |__| |')
+    print(' |_|  |_| _____/ |_____/ ')
+elif ran == 2:
+    print('    __  ________ ____ ')
+    print('   /  |/  / ___// __ \\')
+    print('  / /|_/ /\__ \/ / / /')
+    print(' / /  / /___/ / /_/ / ')
+    print('/_/  /_//____/_____/  ')
+elif ran == 3:
+    print(' __    __     ______     _____    ')
+    print('/\ "-./  \   /\  ___\   /\  __-.  ')
+    print('\ \ \-./\ \  \ \___  \  \ \ \/\ \ ')
+    print(' \ \_\ \ \_\  \/\_____\  \ \____- ')
+    print('  \/_/  \/_/   \/_____/   \/____/ ')
+elif ran == 4:
+    print(' _____ ______   ________  ________     ')
+    print('|\   _ \  _   \|\   ____\|\   ___ \    ')
+    print('\ \  \\\\\__\ \  \ \  \___|\ \  \_|\ \   ')
+    print(' \ \  \\\\|__| \  \ \_____  \ \  \ \\\\ \  ')
+    print('  \ \  \    \ \  \|____|\  \ \  \_\\\\ \ ')
+    print('   \ \__\    \ \__\____\_\  \ \_______\\ ')
+    print('    \|__|     \|__|\_________\|_______|')
+    print('                  \|_________|         ')
+print('==========================================')
+print('          MojangSkinDownload')
+print('            当前版本：MSD1.3')
+print('你可以在Github下载最新版本以及查看最新动态')
+print('==========================================')
 #获取玩家UUID
 name = input('输入Minecraft用户名:')
 url1 = 'https://api.mojang.com/users/profiles/minecraft/' + name    #Mojang API（UUID）
@@ -26,22 +62,38 @@ if skintf != 'slim"':           #判断纤细皮肤
     skin = skin[154+jsu:-13]
 else:
     skin = skin[154+jsu:-68]
+    #print(skin)
 
 #下载文件
+
 print("已经获取玩家皮肤下载地址")
-import requests
+import zipfile
 filename = input('输入皮肤文件保存名称，支持中文:')
-r = requests.get(skin) 
-print('正在下载')
-with open(filename + ".png",'wb') as f:
-    f.write(r.content)
+if filename == type(int):
+    filename = str(filename)
+else:
+    pass  
+from urllib.request import urlretrieve
+urlretrieve(skin,filename + '.png')
 resacce = input("下载成功，按任意键＋回车键关闭,输入res＋回车生成资源包")
 
 #资源包生成
-if resacce != 'res':
-    exit()
-else:
-    import os
+import zipfile
+zipname = input('输入资源包保存名称：')
+copyzip = os.getcwd()
+if resacce == 'res':
+    def zip():
+        startdir = "C:\\msdres"
+        file_news = zipname + '.zip' 
+        z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) 
+        for dirpath, dirnames, filenames in os.walk(startdir):
+            fpath = dirpath.replace(startdir,'')
+            fpath = fpath and fpath + os.sep or ''
+        for filename in filenames:
+            z.write(os.path.join(dirpath, filename),fpath+filename)
+        print ('压缩成功')
+        z.write('pack.mcmeta','pack.mcmeta', zipfile.ZIP_DEFLATED)
+        z.close()
     print('正在生成资源包中')
     os.makedirs('C:\\msdres\\assets\\minecraft\\textures\\entity')
     if skintf != 'slim"':           #判断纤细皮肤
@@ -53,11 +105,10 @@ else:
         f.write("{\n")
     with open("pack.mcmeta","a") as f:
         f.write('  "pack": {\n    "pack_format": 5,\n    "description": "\\u00A7r\\u76ae\\u80a4\\u8d44\\u6e90\\u5305\\u00A7r,§eBy MojangSkinDownload"\n  }\n}')
-    os.system('copy %s %s /Y' % ('pack.mcmeta','C:\\msdres\\'))
-    os.system('cabarc -p -r N a.cab C:\\msdres\\*.*')
-    os.system('copy a.cab Skin.zip /Y')
+    zip()
     os.system('rd /s /q C:\\msdres')
-    os.system('del /F /S /Q a.cab')
     os.system('del /F /S /Q pack.mcmeta')
-    print('生成资源包完成,文件名：Skin.zip')
+    print('生成资源包完成,文件名：' + zipname + '.zip')
     os.system('pause')
+else:
+    exit()
